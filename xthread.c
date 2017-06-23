7605abad06aab86771d0c636f7710de63da42a86
nchaudh2 <nchaudh2@binghamton.edu>
2017-06-23 13:04:03 -0400

    base file
    base file

---
+++ xthread.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "user.h"
+#include "xthread.h"
+
+
+void xthread_create(int * tid, void * (* start_routine)(void *), void * arg)
+{
+    // add your implementation here ...
+    
+}
+
+
+void xthread_exit(void * ret_val_p)
+{
+    // add your implementation here ...
+    
+}
+
+
+void xthread_join(int tid, void ** retval)
+{
+    // add your implementation here ...
+    
+}
